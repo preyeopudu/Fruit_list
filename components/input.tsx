c126ef18a568609fb407c49cc10adcc3c00f54b9
Opudu Preye <preyeopudu@gmail.com>
2022-04-28 14:33:01 +0100

    added search bar

---
+++ components/input.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+interface Props {
+  icon: string;
+  placeholder: string;
+  onChangeText: (text: string) => void;
+}
+
+const Input: FC<Props> = (props) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Ionicons name={props.icon} size={22} color="#555" />
+      </View>
+      <View>
+        <TextInput
+          style={{ paddingHorizontal: 10 }}
+          placeholderTextColor="#555"
+          placeholder={props.placeholder}
+          onChangeText={props.onChangeText}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    width: "100%",
+    paddingHorizontal: 20,
+    borderBottomColor: "#aaaa",
+    borderBottomWidth: 1,
+    alignItems: "center",
+  },
+});
+
+export default Input;
