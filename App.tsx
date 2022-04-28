cb8fe5898fa4681f9e71f59f56eb612cb6ba0d60
Opudu Preye <preyeopudu@gmail.com>
2022-04-28 15:42:05 +0100

    implemented useEffect

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StyleSheet, Text, View, StatusBar, FlatList } from "react-native";
 import Input from "./components/input";
 import ListItem from "./components/ListItem";
-import { Fruits } from "./constants/data";
+import { Fruit, Fruits } from "./constants/data";
 
 export default function App() {
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [fruits, setFruits] = useState<Fruit[]>();
+
+  useEffect(() => {
+    setFruits(Fruits);
+  });
   return (
     <View style={styles.container}>
       <Input
@@ -17,7 +22,7 @@ export default function App() {
       />
 
       <FlatList
-        data={Fruits}
+        data={fruits}
         renderItem={({ item }) => (
           <ListItem price={item.price} name={item.name} id={item.id} />
         )}
